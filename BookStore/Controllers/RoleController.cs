b57afe0d4781b7cfc868b238a7e73dafaa1e9e48
Maxtti12 <gogniatlouis@gmail.com>
2023-08-22 21:29:50 +0700

    first commit

---
+++ BookStore/Controllers/RoleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using BookStore.CustomFilters;
+using BookStore.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookStore.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        [AuthLog(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        [AuthLog(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
